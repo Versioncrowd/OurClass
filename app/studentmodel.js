3333f29012c0d1213779578a39d12bd84f13a8f0
Tarek Alkhatib <tarek.alkhatib1@gmail.com>
2018-02-22 14:48:21 +0100

    correct some stuff

--- app/studentmodel.js
+++ app/studentmodel.js
@@ -2,24 +2,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var studentSchema = new Schema({
-	name: String,
-	quate: String,
-	form: String,
-	living: String,
-	fav_tich: String,
-	img: String,
-	face_link:String,
-	twitt_link:String,
-	linked_link:String
-	//updated: Date
+	
 });
 
-// studentSchema.pre('save', function(next) {
-// 	var currentDate = new Date();
-// 	this.updated = currentDate;
+studentSchema.pre('save', function(next) {
+	var currentDate = new Date();
+	this.updated = currentDate;
 
-// 	next();
-// });
+	next();
+});
 
 var Student = mongoose.model('students', studentSchema);
 module.exports = Student;
