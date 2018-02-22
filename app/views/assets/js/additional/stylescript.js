ec1ffde4ae161e0a400bc93ee626abec7feb43ee
Sabine Gottfried <sabine.gottfried@gmail.com>
2018-02-22 12:39:41 +0100

    latest fe

--- app/views/assets/js/additional/stylescript.js
+++ app/views/assets/js/additional/stylescript.js
@@ -1,20 +1,21 @@
-$(function(){
+$(function() {
   var winH = $(window).height();
-      uppper = $('.upper-bar').innerHeight();
-      nav    =$('.navbar').innerHeight();
-  $('.slider,.carousel-item').height(winH - ( uppper + nav));  
+  uppper = $('.upper-bar').innerHeight();
+  nav = $('.navbar').innerHeight();
+  $('.slider,.carousel-item').height(winH - (uppper + nav));
 
-//# festuers shuffle active
-$('.features-work ul li').on('click',function(){
-$(this).addClass('active').siblings().removeClass('active');
-if($(this).data('class') === 'all'){
-  $('.shuffle-images .col-md').css('opacity', 1);
-}
-else{
-  $('.shuffle-images .col-md').css('opacity','.2');
-  $($(this).data('class')).parent().css('opacity',1)
+  //# festuers shuffle active
+  $('.features-work ul li').on('click', function() {
+    $(this).addClass('active').siblings().removeClass('active');
+    if ($(this).data('class') === 'all') {
+      $('.shuffle-images .col-md').css('opacity', 1);
+    } else {
+      $('.shuffle-images .col-md').css('opacity', '.2');
+      $($(this).data('class')).parent().css('opacity', 1)
 
-}
-}) 
- 
+    }
+  })
+
+  console.log('Hello from stylescript')
+
 })
--- app/views/assets/js/script.js
+++ app/views/assets/js/script.js
@@ -1,7 +1,31 @@
 import sayText from './modules/sayHi.js';
 
+console.log('Hello from script');
+
 $(() => {
 
 sayText('Hello World');
+
+
+  var winH = $(window).height();
+  uppper = $('.upper-bar').innerHeight();
+  nav = $('.navbar').innerHeight();
+  $('.slider,.carousel-item').height(winH - (uppper + nav));
+
+  //# festuers shuffle active
+  $('.features-work ul li').on('click', function() {
+    $(this).addClass('active').siblings().removeClass('active');
+    if ($(this).data('class') === 'all') {
+      $('.shuffle-images .col-md').css('opacity', 1);
+    } else {
+      $('.shuffle-images .col-md').css('opacity', '.2');
+      $($(this).data('class')).parent().css('opacity', 1)
+
+    }
+  })
+
+
+
+
 
 });
